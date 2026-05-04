208d3ff762492f712ec77ad0ac6bd3cd0528acd0
Gregory McCall <gregory@grmccall.com>
2026-05-04 20:41:20 +0200

    use SDL app state

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,40 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_gpu.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3_image/SDL_image.h>
 
-SDL_Window* window;
-SDL_Texture* texture;
-SDL_Renderer* renderer;
+struct AppState {
+  SDL_Window* window;
+  SDL_Texture* texture;
+  SDL_Renderer* renderer;
+};
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
-  window = SDL_CreateWindow("Smiley", 640, 480, SDL_WINDOW_RESIZABLE);
+  SDL_InitSubSystem(SDL_INIT_VIDEO);
+
+  auto state = static_cast<AppState*>(SDL_malloc(sizeof(AppState)));
+
+  state->window = SDL_CreateWindow("Smiley", 640, 480, SDL_WINDOW_RESIZABLE);
+
+  // let SDL choose the rendering backend
+  state->renderer = SDL_CreateRenderer(state->window, nullptr);
 
   SDL_Surface* smiley = IMG_Load("dist/smiley.png");
-
-  // using software renderer because my x220 doesn't support Vulkan
-  renderer = SDL_CreateRenderer(window, nullptr);
-
-  texture = SDL_CreateTextureFromSurface(renderer, smiley);
-
+  state->texture = SDL_CreateTextureFromSurface(state->renderer, smiley);
   SDL_DestroySurface(smiley);
   smiley = nullptr;
 
+  *appstate = state;
   return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppIterate(void* appstate) {
-  SDL_RenderClear(renderer);
-  SDL_RenderTexture(renderer, texture, nullptr, nullptr);
-  SDL_RenderPresent(renderer);
+  auto state = static_cast<AppState*>(appstate);
+
+  SDL_RenderClear(state->renderer);
+  SDL_RenderTexture(state->renderer, state->texture, nullptr, nullptr);
+  SDL_RenderPresent(state->renderer);
 
   return SDL_APP_CONTINUE;
 }
@@ -42,7 +48,11 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 }
 
 void SDL_AppQuit(void* appstate, SDL_AppResult result) {
-  SDL_DestroyWindow(window);
-  SDL_DestroyTexture(texture);
-  SDL_DestroyRenderer(renderer);
+  auto state = static_cast<AppState*>(appstate);
+
+  SDL_DestroyWindow(state->window);
+  SDL_DestroyTexture(state->texture);
+  SDL_DestroyRenderer(state->renderer);
+
+  SDL_free(state);
 }
